572eb07ad47bc662b93a6547de8dedef3216b788
hayrunnisayilbur <145451185+hayrunnisayilbur@users.noreply.github.com>
2023-10-23 14:14:37 +0300

    Add files via upload

---
+++ for dongusu ornek 4-847/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace for_dongusu_ornek_4_847
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbSayilar = new System.Windows.Forms.ListBox();
+            this.txtBaslangic = new System.Windows.Forms.TextBox();
+            this.txtBitis = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.lblBaslangic = new System.Windows.Forms.Label();
+            this.lblBitis = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbSayilar
+            // 
+            this.lbSayilar.FormattingEnabled = true;
+            this.lbSayilar.Location = new System.Drawing.Point(319, 72);
+            this.lbSayilar.Name = "lbSayilar";
+            this.lbSayilar.Size = new System.Drawing.Size(149, 277);
+            this.lbSayilar.TabIndex = 0;
+            // 
+            // txtBaslangic
+            // 
+            this.txtBaslangic.Location = new System.Drawing.Point(152, 98);
+            this.txtBaslangic.Name = "txtBaslangic";
+            this.txtBaslangic.Size = new System.Drawing.Size(140, 20);
+            this.txtBaslangic.TabIndex = 1;
+            this.txtBaslangic.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtBitis
+            // 
+            this.txtBitis.Location = new System.Drawing.Point(152, 163);
+            this.txtBitis.Name = "txtBitis";
+            this.txtBitis.Size = new System.Drawing.Size(140, 20);
+            this.txtBitis.TabIndex = 2;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(152, 203);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(140, 49);
+            this.btnEkle.TabIndex = 3;
+            this.btnEkle.Text = "yaz";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // lblBaslangic
+            // 
+            this.lblBaslangic.AutoSize = true;
+            this.lblBaslangic.Location = new System.Drawing.Point(152, 72);
+            this.lblBaslangic.Name = "lblBaslangic";
+            this.lblBaslangic.Size = new System.Drawing.Size(52, 13);
+            this.lblBaslangic.TabIndex = 4;
+            this.lblBaslangic.Text = "başlangıç";
+            // 
+            // lblBitis
+            // 
+            this.lblBitis.AutoSize = true;
+            this.lblBitis.Location = new System.Drawing.Point(152, 144);
+            this.lblBitis.Name = "lblBitis";
+            this.lblBitis.Size = new System.Drawing.Size(25, 13);
+            this.lblBitis.TabIndex = 5;
+            this.lblBitis.Text = "bitiş";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblBitis);
+            this.Controls.Add(this.lblBaslangic);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtBitis);
+            this.Controls.Add(this.txtBaslangic);
+            this.Controls.Add(this.lbSayilar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbSayilar;
+        private System.Windows.Forms.TextBox txtBaslangic;
+        private System.Windows.Forms.TextBox txtBitis;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Label lblBaslangic;
+        private System.Windows.Forms.Label lblBitis;
+    }
+}
+
---
+++ for dongusu ornek 4-847/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace for_dongusu_ornek_4_847
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            int baslangic, bitis;
+            baslangic = Convert.ToInt32(txtBaslangic.Text);
+            bitis = Convert.ToInt32(txtBitis.Text);
+            for(int i= baslangic; 1 <= bitis; i++)
+            {
+                lbSayilar.Items.Add(i);
+            }
+        }
+    }
+}
